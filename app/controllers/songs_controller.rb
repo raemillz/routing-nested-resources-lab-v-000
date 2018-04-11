f73c8d80ac4f31197add5df7ce193f2dca3db616
Rachel Miller <raeanne1991@gmail.com>
2018-04-11 23:59:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
+        redirect_to artists_path, alert: "Artist not found."
       else
         @songs = @artist.songs
       end
